6f79653bc5fd774876ad15aa7e633dff145822d3
kneskoromny <kneskoromny@gmail.com>
2021-04-30 16:45:39 +0300

    added MVC scheme added IB actions

---
+++ JSONHW/ViewControllers/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  JSONHW
+//
+//  Created by Кирилл Нескоромный on 30.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //MARK: - Override methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    //MARK: - IB Actions
+    @IBAction func buttonOnePressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonTwoPressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonThreePressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonFourPressed(_ sender: UIButton) {
+    }
+    
+    //MARK: - Private Methods
+    private func buttonOnePressed() {
+        
+    }
+    
+    
+}
+
